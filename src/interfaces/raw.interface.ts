44a4eecf05d6a5a60e864b90aab0371fd0333864
mehdi-larek <larek.med@gmail.com>
2025-11-26 15:48:33 +0100

    core: ajout des données d'entrée intermediaire

--- src/interfaces/raw.interface.ts
+++ src/interfaces/raw.interface.ts
@@ -27,3 +27,62 @@ export interface RawInputData {
   technicians: RawTechnician[];
   equipment: RawEquipment[];
 }
+
+//////////////////////////////////////////////////////
+
+export interface RawIntermediaryInputData {
+  laboratory: RawLaboratory;
+  samples: RawIntermediarySample[];
+  technicians: RawIntermediaryTechnician[];
+  equipment: RawIntermediaryEquipment[];
+  constraints: RawIntermediaryConstraint;
+}
+
+export interface RawLaboratory {
+  name: string;
+  openingHours: string;
+  date: string;
+}
+
+export interface RawPatientInfo {
+  age: number;
+  service: string;
+  diagnosis: string;
+}
+
+export interface RawIntermediarySample {
+  id: string;
+  priority: string;
+  type: string;
+  analysisType: string;
+  analysisTime: number;
+  arrivalTime: string;
+  patientInfo: RawPatientInfo;
+}
+
+export interface RawIntermediaryTechnician {
+  id: string;
+  name: string;
+  specialty: string[];
+  efficiency: number;
+  startTime: string;
+  endTime: string;
+  lunchBreak: string;
+}
+
+export interface RawIntermediaryEquipment {
+  id: string;
+  name: string;
+  type: string;
+  compatibleTypes: string[];
+  capacity: number;
+  maintenanceWindow: string;
+  cleaningTime: number;
+}
+
+export interface RawIntermediaryConstraint {
+  maxProcessingTime: number;
+  priorityRules: string[];
+  contaminationPrevention: boolean;
+  parallelProcessing: boolean;
+}
